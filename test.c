971b7d4cfe24606702e6073cd0ce5631eff336c9
cjames <cosminjames555@gmail.com>
2020-12-21 11:31:25 +0200

    Fixed indentation

--- test.c
+++ test.c
@@ -3,29 +3,28 @@
 #include "trycatch.h"
 
 void print_sqrt(float x) {
-	if (x < 0)
-		throw(EX_RANGE);
-	int out = printf("As follows: %g\n", sqrt(x));
-	if (out == -1)
-		throw(EX_EOF);
+    if (x < 0)
+        throw(EX_RANGE);
+    int out = printf("As follows: %g\n", sqrt(x));
+    if (out == -1)
+        throw(EX_EOF);
 }
 
 int main() {
-	
-	try {
-		float f = 0;
-		scanf("%f", &f);
-		print_sqrt(f);
-	} catch(EX_RANGE) {
-		printf("We got a range problem.\n");
-		retry();
-	} catch (EX_EOF) {
-		printf("Something's wrong with the streams.\n");
-		retry();
-	} catch_all() {
-		printf("Something's wrong but aren't sure what.\n");
-		return 1;
-	}
+    try {
+        float f = 0;
+        scanf("%f", &f);
+        print_sqrt(f);
+    } catch(EX_RANGE) {
+        printf("We got a range problem.\n");
+        retry();
+    } catch (EX_EOF) {
+        printf("Something's wrong with the streams.\n");
+        retry();
+    } catch_all() {
+        printf("Something's wrong but aren't sure what.\n");
+        return 1;
+    }
 
-	return 0;
+    return 0;
 }
