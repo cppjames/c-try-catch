dfb649e09ac809ce63d73179cd2812c541a4e809
cjames <cosminjames555@gmail.com>
2020-12-22 09:27:34 +0200

    Change formatting.

--- trycatch.c
+++ trycatch.c
@@ -37,24 +37,24 @@ void _catch_sig_handle(int sig) {
     }
 
     switch (sig) {
-        case SIGFPE:
-            throw(EX_SIGFPE);
-        case SIGILL:
-            throw(EX_SIGILL);
-        case SIGSEGV:
-            throw(EX_SIGSEGV);
-        case SIGABRT:
-            throw(EX_SIGABRT);
-        case SIGINT:
-            throw(EX_SIGINT);
-        case SIGTERM:
-            throw(EX_SIGTERM);
+    case SIGFPE:
+        throw(EX_SIGFPE);
+    case SIGILL:
+        throw(EX_SIGILL);
+    case SIGSEGV:
+        throw(EX_SIGSEGV);
+    case SIGABRT:
+        throw(EX_SIGABRT);
+    case SIGINT:
+        throw(EX_SIGINT);
+    case SIGTERM:
+        throw(EX_SIGTERM);
 #ifdef SIGCONT
-        case SIGCONT:
-            return;
+    case SIGCONT:
+        return;
 #endif
-        default:
-            signal(sig, SIG_DFL);
-            return;
+    default:
+        signal(sig, SIG_DFL);
+        return;
     }
 }
